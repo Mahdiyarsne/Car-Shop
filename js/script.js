f72e7332eff3d0706477ff7dc73416f8f4d53dd1
Mahdiyar Sne <mahdiyarsne98@Gmail.com>
2024-09-22 11:31:06 +0330

    scroll-add

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 window.addEventListener("load", () =>
   document.querySelector(".preloader").classList.add("hidePreloader")
 );
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if ((window, scrollY > 200)) {
+    toTop.classList.add("scroll-top");
+  } else {
+    toTop.classList.remove("scroll-top");
+  }
+});
